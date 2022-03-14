9aaf1be18bc4615e04aafb5d9cdd12c4e0b4abc6
Ayush Mondal <75127242+ayushmondal2008@users.noreply.github.com>
2022-03-14 12:44:58 +0530

    Add files via upload

---
+++ pro108.py
@@ -0,0 +1,8 @@
+import plotly
+import plotly.figure_factory as ff
+import pandas as pd
+import csv
+
+df = pd.read_csv("data.csv")
+fig = ff.create_distplot([df["Avg Rating"].tolist()], ["Avg Rating"])
+plotly.offline.plot(fig)
